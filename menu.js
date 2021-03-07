5cf462334a05105ffbf35461017d8a12af2bdacf
KazKJr <66971386+KazKJr@users.noreply.github.com>
2021-03-06 23:18:30 -0400

    Add files via upload
    Images and CSS should be placed in Folders with those same names.

---
+++ menu.js
@@ -0,0 +1,9 @@
+/**************Compliments TO W3Schools**************/
+function myFunction() {
+  var x = document.getElementById("pop-up-menu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
